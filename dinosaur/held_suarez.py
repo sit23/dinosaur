e7054a3e5469b4403a1c110423773cf4eafda5b2
sit23 <stephen.i.thomson@gmail.com>
2024-07-23 14:45:07 +0000

    Adding first attempt at Lian and Showman forcing.

--- dinosaur/held_suarez.py
+++ dinosaur/held_suarez.py
@@ -148,3 +148,156 @@ class HeldSuarezForcing(time_integration.ExplicitODE):
         temperature_variation=temperature_tendency,
         log_surface_pressure=log_surface_pressure_tendency,
     )
+
+
+class LianShowmanForcing(time_integration.ExplicitODE):
+  """The Lian and Showman problem specification."""
+
+  def __init__(
+      self,
+      coords: coordinate_systems.CoordinateSystem,
+      physics_specs: primitive_equations.PrimitiveEquationsSpecs,
+      reference_temperature: typing.Array,
+      p0: Quantity = 1e5 * units.pascal,
+      sigma_b: Quantity = 0.8,
+      kf: Quantity = 0 / (1 * units.day),
+      ks: Quantity = 1 / (400 * units.day),
+      ka: Quantity = 1 / (40 * units.day),
+      dTy: Quantity = 8 * units.degK,
+      const_t: Quantity = 140.*units.degK,
+      const_theta: Quantity = 155.5*units.degK, 
+      p_crossover = 0.95*1e5*units.pascal,     
+      p_low_taper = 0.4*1e5*units.pascal,
+      p_high_taper = 1.5*1e5*units.pascal,            
+  ):
+    """Initialize Held-Suarez.
+
+    Args:
+      coords: horizontal and vertical descritization.
+      physics_specs: object holding physical constants and definition of custom
+        units to use for initialization of the state.
+      reference_temperature: horizontal reference temperature at all altitudes.
+      p0: reference surface pressure.
+      sigma_b: sigma level of effective planetary boundary layer.
+      kf: coefficient of friction for Rayleigh drag.
+      ka: coefficient of thermal relaxation in upper atmosphere.
+      ks: coefficient of thermal relaxation at earth surface on the equator.
+      minT: lower temperature bound of radiative equilibrium.
+      maxT: upper temperature bound of radiative equilibrium.
+      dTy: horizontal temperature variation of radiative equilibrium.
+      dThz: vertical temperature variation of radiative equilibrium.
+    """
+    self.coords = coords
+    self.physics_specs = physics_specs
+    self.reference_temperature = reference_temperature
+    self.p0 = physics_specs.nondimensionalize(p0)
+    self.sigma_b = sigma_b
+    self.kf = physics_specs.nondimensionalize(kf)
+    self.ka = physics_specs.nondimensionalize(ka)
+    self.ks = physics_specs.nondimensionalize(ks)
+    self.dTy = physics_specs.nondimensionalize(dTy)
+    self.const_t = physics_specs.nondimensionalize(const_t)
+    self.const_theta = physics_specs.nondimensionalize(const_theta)    
+    self.p_crossover = physics_specs.nondimensionalize(p_crossover)    
+    self.p_low_taper = physics_specs.nondimensionalize(p_low_taper)    
+    self.p_high_taper = physics_specs.nondimensionalize(p_high_taper)    
+
+    # Coordinates
+    self.sigma = self.coords.vertical.centers
+    _, sin_lat = self.coords.horizontal.nodal_mesh
+    self.lat = np.arcsin(sin_lat)
+
+    x1 = jnp.log(self.p_high_taper)
+    x2 = jnp.log(self.p_low_taper)
+    D = jnp.log(self.const_t)+ self.physics_specs.kappa*np.log(self.p_crossover)
+    C = jnp.log(self.const_theta)
+
+    matrix_for_cubic = jnp.array([[x1**3, x1**2., x1, 1.0],
+                                [3*(x1**2.), 2*x1, 1.0, 0.0],
+                                [x2**3, x2**2., x2, 1.0],
+                                [3*(x2**2.), 2*x2, 1.0, 0.0]],)
+
+    RHS_of_matrix_equation = jnp.array([C , 0.0, D-(self.physics_specs.kappa*x2), -self.physics_specs.kappa])
+
+    coeffs = jnp.linalg.solve(matrix_for_cubic, RHS_of_matrix_equation)
+
+    self.coeffs = coeffs    
+
+  def kv(self):
+    kv_coeff = self.kf * (
+        np.maximum(0, (self.sigma - self.sigma_b) / (1 - self.sigma_b)))
+    return kv_coeff[:, np.newaxis, np.newaxis]
+
+  def kt(self):
+    cutoff = np.maximum(0, (self.sigma - self.sigma_b) / (1 - self.sigma_b))
+    return self.ka + (self.ks - self.ka) * (
+        cutoff[:, np.newaxis, np.newaxis])
+
+  def equilibrium_temperature(self, nodal_surface_pressure):
+
+    p_values = (
+        self.sigma[:, np.newaxis, np.newaxis] * nodal_surface_pressure)
+
+    temps_from_theta = self.const_theta * (p_values/self.p_crossover)**self.physics_specs.kappa
+
+    background_temp = jnp.zeros_like(p_values)
+
+    where_p_higher = jnp.where(p_values>=self.p_high_taper)[0]
+    background_temp[where_p_higher] = temps_from_theta[where_p_higher]
+
+    where_p_lower = jnp.where(p_values<=self.p_low_taper)[0]
+    background_temp[where_p_lower] = self.const_t
+
+    log_p = jnp.log(p_values)
+
+    full_taper_func = self.coeffs[0]*(log_p**3.) + self.coeffs[1]*(log_p**2.) + self.coeffs[2]*log_p + self.coeffs[3]
+
+    where_inside_taper = jnp.where(jnp.logical_not(jnp.abs(p_values-self.p_crossover)>=(self.p_crossover-self.p_low_taper)))[0]
+
+    background_temp[where_inside_taper] = jnp.exp(full_taper_func[where_inside_taper])* (p_values/self.p_crossover)**self.physics_specs.kappa
+
+    perturbations = self.dTy * np.cos(self.lat)**2
+
+    temperature = background_temp + perturbations
+
+    return temperature
+
+  def explicit_terms(
+      self, state: primitive_equations.State
+  ) -> primitive_equations.State:
+    """Computes explicit tendencies due to Held-Suarez forcing."""
+    aux_state = primitive_equations.compute_diagnostic_state(
+        state=state, coords=self.coords)
+
+    # Nodal velocity tendencies
+    # "velocity" here is `velocity / cos(lat)`
+    nodal_velocity = (
+        jnp.stack(aux_state.cos_lat_u) / self.coords.horizontal.cos_lat**2)
+    nodal_velocity_tendency = -self.kv() * nodal_velocity
+
+    # Nodal temperature tendency
+    nodal_temperature = (
+        self.reference_temperature[:, np.newaxis, np.newaxis] +
+        aux_state.temperature_variation)
+    nodal_log_surface_pressure = self.coords.horizontal.to_nodal(
+        state.log_surface_pressure)
+    nodal_surface_pressure = jnp.exp(nodal_log_surface_pressure)
+    Teq = self.equilibrium_temperature(nodal_surface_pressure)
+    nodal_temperature_tendency = -self.kt() * (nodal_temperature - Teq)
+
+    # Convert to modal
+    temperature_tendency = self.coords.horizontal.to_modal(
+        nodal_temperature_tendency)
+    velocity_tendency = self.coords.horizontal.to_modal(nodal_velocity_tendency)
+    vorticity_tendency = self.coords.horizontal.curl_cos_lat(velocity_tendency)
+    divergence_tendency = self.coords.horizontal.div_cos_lat(velocity_tendency)
+
+    # Zero log_surface_pressure tendency
+    log_surface_pressure_tendency = jnp.zeros_like(state.log_surface_pressure)
+
+    return primitive_equations.State(
+        vorticity=vorticity_tendency,
+        divergence=divergence_tendency,
+        temperature_variation=temperature_tendency,
+        log_surface_pressure=log_surface_pressure_tendency,
+    )
---
+++ notebooks/jupiter/hs_test_jup1.py
@@ -0,0 +1,181 @@
+import functools
+import jax
+import dinosaur
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray
+import pdb
+import time
+
+units = dinosaur.scales.units
+
+def dimensionalize(x: xarray.DataArray, unit: units.Unit) -> xarray.DataArray:
+  """Dimensionalizes `xarray.DataArray`s."""
+  dimensionalize = functools.partial(physics_specs.dimensionalize, unit=unit)
+  return xarray.apply_ufunc(dimensionalize, x)
+
+
+# Formatting trajectory to xarray.Dataset
+def trajectory_to_xarray(coords, trajectory, times):
+
+  trajectory_dict, _ = dinosaur.pytree_utils.as_dict(trajectory)
+  u, v = dinosaur.spherical_harmonic.vor_div_to_uv_nodal(
+      coords.horizontal, trajectory.vorticity, trajectory.divergence)
+  trajectory_dict.update({'u': u, 'v': v})
+  nodal_trajectory_fields = dinosaur.coordinate_systems.maybe_to_nodal(
+      trajectory_dict, coords=coords)
+  trajectory_ds = dinosaur.xarray_utils.data_to_xarray(
+      nodal_trajectory_fields, coords=coords, times=times)
+
+  trajectory_ds['surface_pressure'] = np.exp(trajectory_ds.log_surface_pressure[:, 0, :,:])
+  temperature = dinosaur.xarray_utils.temperature_variation_to_absolute(
+      trajectory_ds.temperature_variation.data, ref_temps)
+  trajectory_ds = trajectory_ds.assign(
+      temperature=(trajectory_ds.temperature_variation.dims, temperature))
+
+  total_layer_ke = coords.horizontal.integrate(u**2 + v**2)
+  total_ke_cumulative = dinosaur.sigma_coordinates.cumulative_sigma_integral(
+      total_layer_ke, coords.vertical, axis=-1)
+  total_ke = total_ke_cumulative[..., -1]
+  trajectory_ds = trajectory_ds.assign(total_kinetic_energy=(('time'), total_ke))
+  return trajectory_ds
+
+def ds_held_suarez_forcing(coords, hs):
+  grid = coords.horizontal
+  sigma = coords.vertical.centers
+  lon, _ = grid.nodal_mesh
+  surface_pressure = physics_specs.nondimensionalize(p0) * np.ones_like(lon)
+  dims = ('sigma', 'lon', 'lat')
+  return xarray.Dataset(
+      data_vars={
+          'surface_pressure': (('lon', 'lat'), surface_pressure),
+          'eq_temp': (dims, hs.equilibrium_temperature(surface_pressure)),
+          'kt': (dims, hs.kt()),
+          'kv': ('sigma', hs.kv()[:, 0, 0]),
+      },
+      coords={'lon': grid.nodal_axes[0] * 180 / np.pi,
+              'lat': np.arcsin(grid.nodal_axes[1]) * 180 / np.pi,
+              'sigma': sigma},
+  )
+
+def linspace_step(start, stop, step):
+  num = round((stop - start) / step) + 1
+  return np.linspace(start, stop, num)
+
+
+#To change this to Jupiter we need to:
+
+# * Find the rotation rate and radius specification - this seems to be in scales.py, then everything is non-dimensionalised for the actual calculations.
+# * Sort out the equilibrium temperatures - perturbations are fine, but basic state is not given analytically in the paper - need to make something up. 
+# * Change drag coeffs etc
+
+print('welcome to dinosaur')
+# Resolution
+
+units = dinosaur.scales.units
+physics_specs = dinosaur.primitive_equations.PrimitiveEquationsSpecs.from_si()
+
+#set simulation parameters
+
+layers = 24
+coords = dinosaur.coordinate_systems.CoordinateSystem(
+    horizontal=dinosaur.spherical_harmonic.Grid.T42(),
+    vertical=dinosaur.sigma_coordinates.SigmaCoordinates.equidistant(layers))
+
+#set physical properties of system
+p0 = 25*100e3 * units.pascal
+
+
+#magnitude of random surface pressure perturbation in initial conditions
+p1 = 5e3 * units.pascal
+
+#set some random perturbation for initial conditions
+rng_key = jax.random.PRNGKey(0)
+
+
+# Set initial state function using resolution, physics specs, p0 and p1
+
+initial_state_fn, aux_features = dinosaur.primitive_equations_states.isothermal_rest_atmosphere(
+    coords=coords,
+    physics_specs=physics_specs,
+    p0=p0,
+    p1=p1)
+
+# set initial state using random perturbation (rng_key) and isothermal rest state
+initial_state = initial_state_fn(rng_key)
+
+#This is a uniform reference temperature, defaulting to 288.
+ref_temps = aux_features[dinosaur.xarray_utils.REF_TEMP_KEY]
+
+#setting orography, which has in fact already been specifed in the isothermal rest atmosphere function, and sits within the aux_features.
+orography = dinosaur.primitive_equations.truncated_modal_orography(
+    aux_features[dinosaur.xarray_utils.OROGRAPHY], coords)
+
+print('now about to do some integrating')
+# Integration settings
+dt_si = 10 * units.minute #timestep
+save_every = 30 * units.day #output frequency
+total_time = 30 * units.day #total time for simulation
+
+inner_steps = int(save_every / dt_si) #number of steps for output
+outer_steps = int(total_time / save_every) #number of steps for entire simulation
+dt = physics_specs.nondimensionalize(dt_si) #non-dimensionalise dt
+
+# Governing equations
+primitive = dinosaur.primitive_equations.PrimitiveEquations(
+    ref_temps,
+    orography,
+    coords,
+    physics_specs)
+
+#Here's where the actual forcing is defined
+hs_forcing = dinosaur.held_suarez.LianShowmanForcing(
+    coords=coords,
+    physics_specs=physics_specs,
+    reference_temperature=ref_temps,
+    p0=p0)
+
+forcing_ds = ds_held_suarez_forcing(coords, hs_forcing)
+
+# Friction coefficient {form-width: "40%"}
+kv = forcing_ds['kv']
+kv_si = dimensionalize(kv, 1 / units.day)
+kv_si.plot(size=5)
+
+pdb.set_trace()
+
+
+#Here's where we tell the time tintegration what equation sets to use, i.e. here a combination of the primitive equations and the hs_forcing fields
+primitive_with_hs = dinosaur.time_integration.compose_equations([primitive, hs_forcing])
+
+#specify the timesteepper, the function to step (primitive_with_hs) and the timestep dt (non-dimensionalised)
+
+step_fn = dinosaur.time_integration.imex_rk_sil3(primitive_with_hs, dt)
+
+#looks like a small scale filter
+filters = [
+    dinosaur.time_integration.exponential_step_filter(
+        coords.horizontal, dt, tau=0.0087504, order=1.5, cutoff=0.8),
+]
+#setting function to step with a filter applied
+step_fn = dinosaur.time_integration.step_with_filters(step_fn, filters)
+
+# Finally we get to the integration function, which is the step_fn and the number of outer and inner steps
+integrate_fn = jax.jit(dinosaur.time_integration.trajectory_from_step(
+    step_fn,
+    outer_steps=outer_steps,
+    inner_steps=inner_steps))
+
+# Define trajectory times, expects start_with_input=False
+times = save_every * np.arange(1, outer_steps+1)
+
+#now actually do some integrating
+print('ready to integrate')
+start_time = time.time()
+final, trajectory = jax.block_until_ready(integrate_fn(initial_state))
+end_time = time.time()
+print(end_time-start_time, ' seconds have passed')
+print('finished integrating')
+
+#turn the output into an xarray dataset
+ds = trajectory_to_xarray(coords, jax.device_get(trajectory), times)
